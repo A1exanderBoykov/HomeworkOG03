54de33c9056940c73c6ac9dc06736cfa978e2fdf
A1exanderBoykov <pups1k89@mail.ru>
2024-04-21 15:02:44 +0300

    add new function

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/2.jpg")
 pygame.display.set_icon(icon)
 
-# Загрузим фоновое изображение
 background_image = pygame.image.load("img/3.jpg")
-# Масштабируем фоновое изображение под размеры окна
+
 background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 target_image = pygame.image.load("img/1.png.png")
@@ -23,11 +22,15 @@ target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+target_speed_x = 0.2
+target_speed_y = 0.2
 
+score = 0
+
+font = pygame.font.Font(None, 44)
 
 running = True
 while running:
-    # Используем фоновое изображение
     screen.blit(background_image, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,7 +40,25 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                score += 1  
+
+
+    target_x += target_speed_x
+    target_y += target_speed_y
+
+
+
+    if target_x <= 0 or target_x + target_width >= SCREEN_WIDTH:
+        target_speed_x = -target_speed_x
+    if target_y <= 0 or target_y + target_height >= SCREEN_HEIGHT:
+        target_speed_y = -target_speed_y
+
+
     screen.blit(target_image, (target_x, target_y))
+
+    score_text = font.render(f"Score: {score}", True, (70, 150, 135))
+    screen.blit(score_text, (10, 10))
+
     pygame.display.update()
 
 pygame.quit()
